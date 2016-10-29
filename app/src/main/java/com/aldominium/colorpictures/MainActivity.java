24b8b92b89eb2731be133a618dbd076cd25bb1ea
aldominium <aldo2122@outlook.com>
2016-10-28 23:25:54 -0500

    1

--- app/src/main/java/com/aldominium/colorpictures/MainActivity.java
+++ app/src/main/java/com/aldominium/colorpictures/MainActivity.java
@@ -1,5 +1,7 @@
 package com.aldominium.colorpictures;
 
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,16 +9,30 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final int PETICION_FOTO = 1;
+    public static final int PETICION_VIDEO = 2;
+    public static final int PETICION_GALERIA_FOTOS = 3;
+    public static final int PETICION_GALERIA_VIDEOS = 4;
+
+    public static final int MEDIA_FOTO = 5;
+    public static final int MEDIA_VIDEO = 6;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
+    
+
 
     public void tomarFoto(View view) {
 
-        Toast.makeText(this, "Foto!", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        startActivityForResult(intent,PETICION_FOTO);
+
 
     }
 
