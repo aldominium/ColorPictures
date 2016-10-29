8093afaffa1a4e9d8022d7a02de4e6c9bd55b1ef
aldominium <aldo2122@outlook.com>
2016-10-28 21:33:14 -0500

    1

--- app/src/main/java/com/aldominium/colorpictures/MainActivity.java
+++ app/src/main/java/com/aldominium/colorpictures/MainActivity.java
@@ -2,6 +2,8 @@ package com.aldominium.colorpictures;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void tomarFoto(View view) {
+
+        Toast.makeText(this, "Foto!", Toast.LENGTH_SHORT).show();
+        
+    }
+
+    public void tomarVideo(View view) {
+        Toast.makeText(this, "Video!", Toast.LENGTH_SHORT).show();
+    }
+
+    public void verGaleriaFotos(View view) {
+        Toast.makeText(this, "Galeria de Fotos!", Toast.LENGTH_SHORT).show();
+    }
+
+    public void verGaleriaVideos(View view) {
+        Toast.makeText(this, "Galeria de Video!", Toast.LENGTH_SHORT).show();
     }
 }
