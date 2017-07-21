360baa4f8d68625f6daa6ddf70c964d84f8bcfa0
Aldo Olivares <aldo2122@outlook.com>
2017-07-21 00:37:26 -0500

    Update to Support file on Android Nougat

--- app/src/main/java/com/aldominium/colorpictures/MainActivity.java
+++ app/src/main/java/com/aldominium/colorpictures/MainActivity.java
@@ -1,6 +1,7 @@
 package com.aldominium.colorpictures;
 
 import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.media.MediaScannerConnection;
@@ -8,6 +9,8 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -20,7 +23,7 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     public static final int PETICION_FOTO = 1;
     public static final int PETICION_VIDEO = 2;
@@ -32,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
     private static final int MAX_DURATION = 30;
     private static final String TAG = MainActivity.class.getSimpleName();
     private static final int CAMERA_WRITE_PERMISSION = 11;
+    private static final int VIDEO_WRITE_PERMISSION = 12;
 
     private Uri mediaUri;
 
@@ -53,13 +57,14 @@ public class MainActivity extends AppCompatActivity {
                 //Ver la foto
                 Intent intent = new Intent(this,ImageActivity.class);
                 intent.setData(mediaUri);
+                //intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
                 startActivity(intent);
             }
 
             if (requestCode == PETICION_VIDEO){
 
                 Intent intent = new Intent(Intent.ACTION_VIEW, mediaUri);
-
+                //intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
                 intent.setDataAndType(mediaUri,"video/*");
 
                 startActivity(intent);
@@ -94,8 +99,7 @@ public class MainActivity extends AppCompatActivity {
             if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
 
                 if (shouldShowRequestPermissionRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE)){
-                    mostrarExplicacion();
-                    requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},CAMERA_WRITE_PERMISSION);
+                    mostrarExplicacion(PETICION_FOTO);
                 }else{
                     requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},CAMERA_WRITE_PERMISSION);
                 }
@@ -114,20 +118,75 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void mostrarExplicacion() {
+    private void mostrarExplicacion(final int tipoPeticion) {
 
         new AlertDialog.Builder(this)
                 .setTitle("Necesito tu permiso")
                 .setMessage("Necesito tu permiso para poder guardar tus fotos y videos")
-                .setNeutralButton("Ok", null)
+                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if (tipoPeticion == PETICION_FOTO){
+                            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},CAMERA_WRITE_PERMISSION);
+                        }else if(tipoPeticion == PETICION_VIDEO){
+                            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},VIDEO_WRITE_PERMISSION);
+                        }else {
+                            throw new IllegalArgumentException();
+                        }
+                    }
+                })
+                .setNegativeButton("Nop", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this, "ouch", Toast.LENGTH_SHORT).show();
+                    }
+                })
                 .show();
 
     }
 
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+
+            if (requestCode == CAMERA_WRITE_PERMISSION){
+                crearMedio(PETICION_FOTO);
+            }
+
+            if(requestCode == VIDEO_WRITE_PERMISSION){
+
+                crearMedio(PETICION_VIDEO);
+
+            }
+
+        }
+
+
+    }
+
     public void tomarVideo(View view) {
 
-        crearMedio(PETICION_VIDEO);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+
+            if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+
+                if (shouldShowRequestPermissionRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE)){
+                    mostrarExplicacion(PETICION_VIDEO);
+                }else{
+                    requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},VIDEO_WRITE_PERMISSION);
+                }
+
+
+            }else{
+                crearMedio(PETICION_VIDEO);
+            }
+
+        }else {
+
+            crearMedio(PETICION_VIDEO);
+        }
 
     }
 
@@ -224,7 +283,9 @@ public class MainActivity extends AppCompatActivity {
 
             MediaScannerConnection.scanFile(this, new String[] { archivo.getPath() }, new String[] { "image/jpeg","video/mp4" }, null);
 
-            return Uri.fromFile(archivo);
+            //return Uri.fromFile(archivo);//# Linea Obsoleta
+
+            return FileProvider.getUriForFile(this, this.getApplicationContext().getPackageName() + ".com.aldominium.provider", archivo);
 
 
         }else if(tipoMedio == MEDIA_VIDEO){
@@ -239,7 +300,9 @@ public class MainActivity extends AppCompatActivity {
 
             MediaScannerConnection.scanFile(this, new String[] { archivo.getPath() }, new String[] { "image/jpeg","video/mp4" }, null);
 
-            return Uri.fromFile(archivo);
+            //return Uri.fromFile(archivo); //# Linea Obsoleta
+
+            return FileProvider.getUriForFile(this, this.getApplicationContext().getPackageName() + ".com.aldominium.provider", archivo);
 
 
 
